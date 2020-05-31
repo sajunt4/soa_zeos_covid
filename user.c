cda174f41d9a0fb4e691785c88fd54f473b932e0
abel <sajunt4@gmail.com>
2020-05-31 22:09:54 +0200

    Added menu to the user execution flow

--- user.c
+++ user.c
@@ -1,13 +1,20 @@
 #include <game.h>
+#include <menu.h>
 
 //TODO: Colisiones
 
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
+    int result;
+    while((result = updateMenu()) == -1);
 
-
-    gameLoop();
+    if(result == 0){
+        //Run tests here.
+    }
+    else if(result == 1){
+        gameLoop();
+    }
 
     while(1) {
 
