ac8c6f1a14b36513288d291a12d31b284926c244
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2020-05-31 23:20:32 +0200

    Execute tests

--- user.c
+++ user.c
@@ -4,22 +4,21 @@
 //TODO: Colisiones
 
 void test_sbrk(){
-    int size = 1024*4;
-    int *res = sbrk(size*3);
-    int pid = fork();
-    int *res2 = sbrk(size*4);
-    for (int i = 0; i < size*3; ++i) {
+    write(1, "Test: SBRK\n", strlen("Test: SBRK\n"));
+    int *res = sbrk(1024*4*1);
+    if((int)res < 0){
+        write(1, "Error found!!!!", strlen("Error found!!!!"));
+        perror();
+        exit(1);
+    }
+    for (int i = 0; i < 1024 * 1; ++i) {
         res[i] = i;
     }
-    for (int j = 0; j < size*4; ++j) {
-        res2[j] = j;
-    }
-    res = sbrk(-20);
-    if ((int) res < 0) perror();
     write(1, "Test PASSED: SBRK\n", strlen("Test PASSED: SBRK\n"));
 }
 
 void test_get_key(){
+    write(1, "Test: GET_KEY\n", strlen("Test: GET_KEY\n"));
     char *c;
     for (int i = 0; i < 10; ++i) {
         int res = get_key(c);
@@ -33,6 +32,7 @@ void test_get_key(){
 }
 
 void test_put_screen(){
+    write(1, "Test: PUT_SCREEN\n", strlen("Test: PUT_SCREEN\n"));
     char buffer[25][80];
     for(int x = 0; x < 80; x++)
         for(int y = 0; y < 25; y++)
@@ -62,7 +62,11 @@ int __attribute__ ((__section__(".text.main")))
     while((result = updateMenu()) == -1);
 
     if(result == 0){
-        //Run tests here.
+        write(1, "Test STARTING\n", strlen("Test STARTING\n"));
+        test_put_screen();
+        test_sbrk();
+        test_get_key();
+        write(1, "All Tests PASSED\n", strlen("All Tests PASSED\n"));
     }
     else if(result == 1){
         gameLoop();
