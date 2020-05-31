8ae1213261fe3e706048db1d632fa7c9d399b4b0
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2020-05-31 22:40:20 +0200

    Added test_put_screen()

--- user.c
+++ user.c
@@ -28,6 +28,31 @@ void test_get_key(){
             write(1, "Key Found\n", strlen("Key Found\n"));
         }
     }
+
+    write(1, "Test PASSED: GET_KEY\n", strlen("Test PASSED: GET_KEY\n"));
+}
+
+void test_put_screen(){
+    char buffer[25][80];
+    for(int x = 0; x < 80; x++)
+        for(int y = 0; y < 25; y++)
+            buffer[y][x] = ' ';
+
+    char wrongBuffer[1][1];
+    int res = put_screen(wrongBuffer);
+    if(res < 0) perror();
+
+    for(int x = 0; x < 80/2; x++){
+        for(int y = 0; y < 25/2; y++){
+            buffer[y][x] = 'O';
+            buffer[25-y][80-x] = 'O';
+
+            int res = put_screen(buffer);
+            if(res < 0) perror();
+        }
+    }
+
+    write(1, "Test PASSED: PUT_SCREEN\n", strlen("Test PASSED: PUT_SCREEN\n"));
 }
 
 int __attribute__ ((__section__(".text.main")))
